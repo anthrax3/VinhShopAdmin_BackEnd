5b2973fffe4e0e481a3e76373faaddab9b05c75a
DESKTOP-JL6O5JL\VinhDeveloper <quangvinh050293@gmail.com>
2017-09-22 16:39:38 +0700

    Init project

---
+++ VinhShopApi/App_Start/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(VinhShopApi.App_Start.Startup))]
+
+namespace VinhShopApi.App_Start
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ VinhShopApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace VinhShopApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
